c0684df0c67373608f8c30130ab6b447098d8d55
Some useless programmer <117359037+batyrnurkhan@users.noreply.github.com>
2024-04-08 11:45:40 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# Peer-graded-Assignment-Course-Project-Shiny-Application-and-Reproducible-Pitch
-Peer-graded Assignment: Course Project: Shiny Application and Reproducible Pitch
-
-This peer assessed assignment has two parts. 
-
-First, you will create a Shiny application and deploy it on Rstudio's servers. 
-
-Second, you will use Slidify or Rstudio Presenter to prepare a reproducible pitch presentation about your application.
-
-Click the Below Link for the Application.
-
-https://bkarmay.shinyapps.io/PricePredictorForDiamonds/
-
-Click the Below Link for the Rpubs presentation.
-
-http://rpubs.com/bkarmay/338599
-
+# Peer-graded-Assignment-Final-Project-Submission
+The goal of this exercise is to create a product to highlight the prediction algorithm that we have built and to provide an interface that can be accessed by others
--- ui.R
+++ ui.R
@@ -1,37 +1,107 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that realizes single word prediction
 shinyUI(fluidPage(
 
-        # Application title
-        titlePanel("Price predictor for Diamonds"),
+    # Application title
+    titlePanel("SINGLE WORD PREDICTION - Lite Version"),
 
-        # Sidebar with options selectors
-        sidebarLayout(
-                sidebarPanel(
-                        helpText("This application is a predictor for the price of a diamond based on its characteristics."),
-                        h3(helpText("Select:")),
-                        numericInput("car", label = h4("Carats"), step = 0.01, value = 1.5),
-                        selectInput("cut", label = h4("Cut"),
-                                    choices = list("Unknown" = "*", "Fair" = "Fair", "Good" = "^Good",
-                                                   "Very Good" = "Very Good", "Premium" = "Premium",
-                                                   "Ideal" = "Ideal")),
-                        selectInput("col", label = h4("Color"),
-                                    choices = list("Unknown" = "*", "D" = "D", "E" = "E",
-                                                   "F" = "F", "G" ="G",
-                                                   "H" = "H", "I" = "I",
-                                                   "J" = "J")),
-                        selectInput("clar", label = h4("Clarity"),
-                                    choices = list("Unknown" = "*", "I1" = "I1", "SI2" = "SI2",
-                                                   "SI1" = "SI1", "VS2" = "VS2", "VS1" = "VS1",
-                                                   "VVS2" = "VVS2", "VVS1" = "VVS1", "IF" = "IF" ))
-                ),
-
-                # Show a plot with diamonds and regression line
-                mainPanel(
-                        plotOutput("distPlot"),
-                        h4("Predicted value of this diamond is:"),
-                        h3(textOutput("result"))
-                )
+    # Application subtitle
+    fluidRow(
+        column(12,
+               h4("based on n-gram model for News articles or Twitter text data")
         )
+    ),
+    
+    tags$hr(style="border-color: black;"),
+    
+    # Sidebar panel with: select box to select n-gram model and with help text ----
+    sidebarPanel(
+        
+        
+        # Select box with a select input for each n-gram model
+        selectInput("model", label = "Select 4-gram model for:", 
+                    choices = list("News articles data" = 1, "Twitter data" = 2), 
+                    selected = 1),
+        
+        # Include clarifying text ----
+        helpText("Select 4-gram model calculated based on content of text file in English language. 
+             First text file contains News articles data, second text file contains Twitter data."),
+        
+        tags$hr(style="border-color: black;"),
+        
+        tags$b("About:"),
+        helpText("Both text files come from Coursera-SwiftKey dataset 
+             (version downloaded 27th Sept 2019)."),
+        helpText("Application contains 4-gram model for each among two text data files. 
+             Each n-grams set is without n-grams occured with frequency equals one."),
+        helpText("This is lite version of application 
+             just to demonstrate ability to predict next word, with good accuracy, 
+             based on entered text phrase in English language."),
+        tags$a(href="https://eg.linkedin.com/in/ahmedzard", "Author"),
+        
+        tags$hr(style="border-color: black;"),
+        
+        tags$b("Usage example:"),
+        helpText("Entered text phrase:"), tags$i("I'm open to that and a lot of"),
+        helpText("Cleaned text phrase:"), tags$i("open lot"),
+        helpText("Predicted next word - News articles:"), tags$i("things"),
+        helpText("Predicted next word - Twitter data:"), tags$i("doors")
+    ),
+    
+    
+    # Main panel with: input for enter text phrase, submit button, 
+    # output for cleaned text phrase and output for single word prediction
+    mainPanel(
+        
+        
+        # Input box to enter text phrase without last word
+        br(),  
+        textAreaInput("textInput", label = "Enter text phrase without last word:", value = "", width = "100%", rows = 6),
+        
+        # Include clarifying text ----
+        helpText("Mentioned last word will be predicted and displayed in last output text box."),
+        
+        helpText("This input box can be resized by move bottom right corner."),
+        
+        hr(),
+        br(),
+        
+        # Action button to submit entered text phrase
+        actionButton("action", label = tags$b("Clean & Predict"), width = "25%"),
+        
+        # Action button to clear entered text phrase
+        actionButton("reset", label = tags$b("Clear"), width = "25%"),
+        
+        # Include clarifying text ----
+        helpText("Press left button to clean entered text phrase and predict next word."),
+        helpText("Edit entered text phrase to see new cleaned text phrase and new predicted next word or"),
+        helpText("press right button to clear entered and cleaned text phrase and to clear predicted next word."),
+        
+        hr(),
+        br(),
+        
+        tags$b("Cleaned text phrase:"),
+        
+        # Output box to display cleaned text phrase
+        verbatimTextOutput("cleanedText", placeholder = TRUE),
+        
+        br(),
+        
+        tags$b("Predicted next word:"),
+        
+        # Output box to display predicted word
+        fluidRow(column(4, verbatimTextOutput("predictedWord", placeholder = TRUE)))
+        
+    )
+    
 ))
